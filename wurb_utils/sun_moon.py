8a6a4f12550e178b2b857e75509ccd6145c107e0
Arnold Andreasson <arnold@mellifica.se>
2026-03-07 13:04:30 +0100

    Minor corrections.

--- wurb_utils/sun_moon.py
+++ wurb_utils/sun_moon.py
@@ -142,11 +142,11 @@ class SunMoon:
             elif (moon_phase >= 14.00 - delta) and (moon_phase < 14.00 + delta):
                 phase_name = "Full moon"
             elif (moon_phase >= 17.50 - delta) and (moon_phase < 17.50 + delta):
-                phase_name = "Wanning gibbous"
+                phase_name = "Waning gibbous"
             elif (moon_phase >= 21.00 - delta) and (moon_phase < 21.00 + delta):
                 phase_name = "Last quarter"
             elif (moon_phase >= 24.50 - delta) and (moon_phase < 24.5 + delta):
-                phase_name = "Wanning crescent"
+                phase_name = "Waning crescent"
             elif (moon_phase >= 28.00 - delta) and (moon_phase < 28.00):
                 phase_name = "New moon"
         else:
